f6c74bd17f406e9fcfce1555899328934e74b780
Billy Lowry <billyn.lowry@gmail.com>
2023-04-26 13:39:48 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(n, e){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: n,
+            email: e
+        })
+    })
+            .then(resp => resp.json())
+            .then((obj) => {
+                let objID = obj.id;
+                console.log("obj:", obj)
+                console.log("objID:", objID)
+                document.body.innerHTML = obj.id;
+            })
+            .catch(function(error) {
+                document.body.innerHTML = error.message;
+              });
+            
+}
